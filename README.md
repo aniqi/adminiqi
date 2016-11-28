1a30a87a687bf770f8e439b37f027658294cd73e
Andrey Niqi <petrovvich@gmail.com>
2016-11-28 21:14:02 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# adminiqi
-package for laravel adnistartor panel
+# Adminiqi
+
+
+## Install
+
+Via Composer
+
+``` bash
+"Aniqi/Adminiqi":"0.1.*"
+```
+
+## Usage
+
+``` php
+php artisan vendor:publish
+php artisan migrate
+```
+
---
+++ src/AdminiqiServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Aniqi\Adminiqi;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class AdminiqiServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        require __DIR__.'/routes.php';
+    
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
